b99caf86bf71a9c82b8391104a434d6a8c5b7d9b
andreshermoso <65416045+andreshermoso@users.noreply.github.com>
2025-07-29 15:33:31 -0500

    Update readme.m

--- sap-ase-16-install-rhel/readme.m
+++ sap-ase-16-install-rhel/readme.m
@@ -1,1 +1,21 @@
+---
 
+## :bookmark_tabs: devops-with-chatgpt/README.md
+
+```md
+# DevOps with ChatGPT
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+## Overview
+A curated collection of ChatGPT prompts and responses illustrating how AI can accelerate DevOps workflows: from AWS infra provisioning to CI/CD pipeline automation.
+
+## Table of Contents
+1. [Repo Structure](#repo-structure)
+2. [Getting Started](#getting-started)
+3. [Categories & Prompts](#categories--prompts)
+4. [How to Use](#how-to-use)
+5. [Contributing](#contributing)
+6. [License](#license)
+
+## Repo Structure
